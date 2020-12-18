ebe4f9c0988cfa370f9f5fda14e4535495fe8ea8
Koke <jorge.arancibia.jeal@gmail.com>
2020-12-18 04:42:24 -0400

    Build renew

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
+
 require('dotenv').config();
 const cors = require('cors');
 
@@ -12,14 +11,8 @@ var app = express();
 //CORS
 app.use(cors());
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static("public"));
 
