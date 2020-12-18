5aa92c405b8b56b575480e0b2d1a2eeaae330e62
koke <jorge.arancibia.jeal@gmail.com>
2020-12-18 11:41:35 -0300

    Change build

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static("public"));
 
 //Rutas
 app.use('/api/productos', require('./routes/productsRoutes'));
