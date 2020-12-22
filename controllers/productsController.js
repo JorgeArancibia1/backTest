1a62a137cf1fb691938cbac97d9c334de69fe9db
koke <jorge.arancibia.jeal@gmail.com>
2020-12-22 01:36:48 -0300

    env added correctly8

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,53 +1,45 @@
 const axios = require("axios");
 
 const getProducts = async (req, res) => {
-	// var config = {
-	//   method: 'get',
-	//   url: 'https://simple.ripley.cl/api/v2/products?format=json&partNumbers=2000379450763'
-	// };
+	console.log("Entró ");
 
-	// axios(config)
-	// .then(function (response) {
-	//   console.log(JSON.stringify({
-	// 	ok: true,
-	// 	fullImage: data[0].fullImage,
-	// 	images: data[0].images,
-	// 	description: data[0],
-	// 	prices: data[0].prices,
-	// 	warranties: data[0].warranties[0].Price[0],
-	// }));
-	// })
-	// .catch(function (error) {
-	//   console.log(error);
-	// });
+	try {
+		const { data } = await axios.get(
+			`https://simple.ripley.cl/api/v2/products?format=json&partNumbers=2000379450763`,
+			{
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+					"Content-Type": "application/json",
+					Authorization: "simple.ripley.cl",
+					Accept: "application/json",
+				},
+			}
+		);
 
-	const { data } = await axios.get(
-		`https://simple.ripley.cl/api/v2/products?format=json&partNumbers=2000379450763`,
-		{
-			headers: {
-				"Access-Control-Allow-Origin": "*",
-				"Content-Type": "application/json",
-				"Authorization": "simple.ripley.cl",
-				"Accept": "application/json",
-			},
+		console.log("After ");
+
+		if (!data) {
+			res.status(403).json({
+				ok: false,
+				error: "Error al traer la data.",
+			});
 		}
-	);
 
-	if (!data) {
-		res.status(403).json({
-			ok: false,
-			error: "Error al traer la data.",
+		res.status(200).json({
+			ok: true,
+			fullImage: data[0].fullImage,
+			images: data[0].images,
+			description: data[0],
+			prices: data[0].prices,
+			warranties: data[0].warranties[0].Price[0],
 		});
-	}
-
-	res.status(200).json({
-		ok: true,
-		fullImage: data[0].fullImage,
-		images: data[0].images,
-		description: data[0],
-		prices: data[0].prices,
-		warranties: data[0].warranties[0].Price[0],
-	});
+	} catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      error: "Error en el servicio.",
+    });
+  }
 };
 
 module.exports = {
